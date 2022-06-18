74f16860a4bf4cbcb01cfe1cd9f1fb2fca95e90a
Alexandr Kolomytski <alexandr.kolom.web@yandex.ru>
2022-06-18 16:51:29 +1000

    ex17 arrow function, last in part

---
+++ Part1/17/arrow.js
@@ -0,0 +1,5 @@
+const ask = (question, yes, no) => confirm(question) ? yes() : no();
+
+ask("Вы согласны?",
+function() { alert("Вы согласились."); },
+function() { alert("Вы отменили выполнение."); })
