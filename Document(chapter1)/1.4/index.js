6044d948709569732784250b99843b1fe45cd335
Alexandr Kolomytski <alexandr.kolom.web@yandex.ru>
2022-06-21 16:16:28 +1000

    Поиск: getElement*, querySelector*

---
+++ Document(chapter1)/1.4/index.js
@@ -0,0 +1,9 @@
+//4.1
+const table = document.getElementById('age-table');
+const labels = document.querySelectorAll('label');
+const firstAge = document.querySelector('td[name="age"]');
+const formSearch = document.querySelector('form[name="search"]');
+const fInput = formSearch.querySelector('input');
+const inputs = formSearch.querySelectorAll('input');
+const lastInput = inputs[inputs.length - 1];
+
